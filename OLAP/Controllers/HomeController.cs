e000ae4e093eebd030f4a763a1cb7b35f205e7fa
Igor Goremykin <c-sharp@bk.ru>
2013-06-22 23:21:56 +0300

    Рабочая форма отправки

--- OLAP/Controllers/HomeController.cs
+++ OLAP/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.IO;
+using System.Text;
 
 namespace OLAP.Controllers
 {
@@ -19,6 +21,19 @@ namespace OLAP.Controllers
         public ActionResult Manage()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddDataBase(HttpPostedFileBase filePath, string name)
+        {
+            string path = Server.MapPath("~/Files");
+            if (filePath != null)
+            {
+                var serverName = new StringBuilder(Guid.NewGuid().ToString());
+                serverName.Append(filePath.FileName, filePath.FileName.Length - 4, 4);
+                filePath.SaveAs(Path.Combine(path, serverName.ToString()));
+            }
+            return RedirectToAction("Manage");
         }
     }
 }
